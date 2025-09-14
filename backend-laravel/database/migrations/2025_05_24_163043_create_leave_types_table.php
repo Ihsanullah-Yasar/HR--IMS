6629f75234b0bca5a6079e5d6c8337114fed428d
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-09-14 22:01:22 +0430

    Adjust create_leave_types_table migration

--- backend-laravel/database/migrations/2025_05_24_163043_create_leave_types_table.php
+++ backend-laravel/database/migrations/2025_05_24_163043_create_leave_types_table.php
@@ -11,8 +11,15 @@ return new class extends Migration {
             $table->id();
             $table->string('code')->unique();
             $table->jsonb('name');
+            $table->text('description')->nullable();
             $table->integer('days_per_year');
             $table->boolean('is_paid')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->string('color')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }

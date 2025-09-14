891424048887ccde66d35324a00f8c6bc036f184
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-09-14 22:01:19 +0430

    Update LeaveResource: format API response for leave requests

--- backend-laravel/app/Http/Resources/LeaveResource.php
+++ backend-laravel/app/Http/Resources/LeaveResource.php
@@ -25,10 +25,35 @@ class LeaveResource extends JsonResource
             'status' => $this->status,
             'approved_by' => $this->approved_by,
             'approved_at' => $this->approved_at?->toISOString(),
+            'created_by' => $this->whenLoaded('createdBy', function () {
+                return $this->createdBy ? [
+                    'id' => $this->createdBy->id,
+                    'name' => $this->createdBy->name,
+                ] : null;
+            }),
+            'updated_by' => $this->whenLoaded('updatedBy', function () {
+                return $this->updatedBy ? [
+                    'id' => $this->updatedBy->id,
+                    'name' => $this->updatedBy->name,
+                ] : null;
+            }),
+            'deleted_by' => $this->whenLoaded('deletedBy', function () {
+                return $this->deletedBy ? [
+                    'id' => $this->deletedBy->id,
+                    'name' => $this->deletedBy->name,
+                ] : null;
+            }),
             'created_at' => $this->created_at?->toISOString(),
             'updated_at' => $this->updated_at?->toISOString(),
+            'deleted_at' => $this->deleted_at?->toISOString(),
             'employee' => $this->whenLoaded('employee'),
             'leaveType' => $this->whenLoaded('leaveType'),
+            'approvedBy' => $this->whenLoaded('approvedBy', function () {
+                return $this->approvedBy ? [
+                    'id' => $this->approvedBy->id,
+                    'name' => $this->approvedBy->name,
+                ] : null;
+            }),
         ];
     }
 }

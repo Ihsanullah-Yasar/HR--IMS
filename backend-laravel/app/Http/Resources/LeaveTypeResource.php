7995391799ee92fcf4c6d21a10d814af8035748e
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-09-14 22:01:20 +0430

    Update LeaveTypeResource: format API response for leave types

--- backend-laravel/app/Http/Resources/LeaveTypeResource.php
+++ backend-laravel/app/Http/Resources/LeaveTypeResource.php
@@ -16,13 +16,34 @@ class LeaveTypeResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'code' => $this->code,
             'name' => $this->name,
             'description' => $this->description,
-            'default_days' => $this->default_days,
+            'days_per_year' => $this->days_per_year,
+            'is_paid' => $this->is_paid,
             'is_active' => $this->is_active,
             'color' => $this->color,
+            'created_by' => $this->whenLoaded('createdBy', function () {
+                return $this->createdBy ? [
+                    'id' => $this->createdBy->id,
+                    'name' => $this->createdBy->name,
+                ] : null;
+            }),
+            'updated_by' => $this->whenLoaded('updatedBy', function () {
+                return $this->updatedBy ? [
+                    'id' => $this->updatedBy->id,
+                    'name' => $this->updatedBy->name,
+                ] : null;
+            }),
+            'deleted_by' => $this->whenLoaded('deletedBy', function () {
+                return $this->deletedBy ? [
+                    'id' => $this->deletedBy->id,
+                    'name' => $this->deletedBy->name,
+                ] : null;
+            }),
             'created_at' => $this->created_at?->toISOString(),
             'updated_at' => $this->updated_at?->toISOString(),
+            'deleted_at' => $this->deleted_at?->toISOString(),
         ];
     }
 }
